675681e138c63a43fa2cd49c6bc05b9a710a9b5b
Mohamed Shihab Aaqil ESR <30803934+mdaaqil@users.noreply.github.com>
2022-06-19 13:25:48 +0530

    Update TimeBasedMap.java

--- src/com/coding/patterns/hashmap/TimeBasedMap.java
+++ src/com/coding/patterns/hashmap/TimeBasedMap.java
@@ -1,53 +1,50 @@
-package com.coding.patterns.hashmap;
+class TimeMap {
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-class Pair {
-    public String value;
-    public int time;
-
-    public Pair(String value, int time) {
-        this.value = value;
-        this.time = time;
+    class TimeValue{
+        String val;
+        int timeStamp;
+        public TimeValue(String val, int time)
+        {
+            this.val = val;
+            timeStamp = time;
+        }
     }
-}
-public class TimeBasedMap {
-    Map<String, List<Pair>> timeMap;
-
-    public TimeBasedMap() {
-        timeMap = new HashMap<>();
+    Map<String, List<TimeValue>> map;
+    public TimeMap() {
+        map = new HashMap<>();
     }
-
+    
     public void set(String key, String value, int timestamp) {
-        if (!timeMap.containsKey(key)) {
-            timeMap.put(key, new ArrayList<>());
-        }
-        timeMap.get(key).add(new Pair(value, timestamp));
+        map.computeIfAbsent(key, k-> new ArrayList<>()).add(new TimeValue(value, timestamp));
     }
-
+    
     public String get(String key, int timestamp) {
-        List<Pair> values = timeMap.getOrDefault(key, new ArrayList<>());
-        String result = "";
-
-        int l = 0, r = values.size() - 1;
-
-        while (l <= r) {
-            int m = (l + r) / 2;
-            if (values.get(m).time == timestamp) {
-                result = values.get(m).value;
-                return result;
+        List<TimeValue> list = map.getOrDefault(key, null);
+        if(list == null) return "";
+        int l = 0, r = list.size() - 1;
+        String res = "";
+        while(l <= r)
+        {
+            int mid = (l + r) / 2;
+            if(list.get(mid).timeStamp == timestamp) 
+            {
+                return list.get(mid).val;
             }
-            else if (values.get(m).time < timestamp) {
-                result = values.get(m).value;
-                l = m + 1;
+            if(list.get(mid).timeStamp < timestamp)
+            {
+                res = list.get(mid).val;
+                l = mid + 1;
             }
-            else {
-                r = m - 1;
-            }
+            else
+                r = mid - 1;
         }
-        return result;
+        return res;
     }
 }
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
